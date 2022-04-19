5071a57a9036a77081126846af7229455b26ef3b
jose luis munoz <joseluismunozzuniga@gmail.com>
2022-04-19 16:53:33 -0400

    first commit

--- src/database.ts
+++ src/database.ts
@@ -1,8 +1,13 @@
 import mongoose from "mongoose";
 import config from "./config";
 (async ()=> {
+
     try {
-        const db=  await mongoose.connect('mongodb://localhos/mern-database');
+        const options = {
+            user:config.MONGO_USER,
+            password:config.MONGO_PASSWORD
+        }
+        const db=  await mongoose.connect(`mongodb://${config.MONGO_HOST}/${config.MONGO_DATABASE}`,options);
         console.log('Database is conected ');
         
     } catch (error) {
